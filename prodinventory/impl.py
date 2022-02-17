930c97fbd4856c37986e4597af224352fe9d0db1
pandit dandgule <panditdandgule777@gmail.com>
2022-02-17 13:00:51 +0530

    new project added

---
+++ prodinventory/impl.py
@@ -0,0 +1,266 @@
+import sys
+from ecominventory.services import ProductServices
+from ecominventory.productinfo import Product
+from myerror import MyError
+
+
+class ValueDivision(Exception):
+    def __init__(self, arg):
+        self.msg = arg
+
+    def __str__(self):
+        return repr(self.msg)
+
+
+class ProductServiceImpl(ProductServices, ValueDivision):
+    productList = []  # class level var --> ProductServiceImpl.productList
+    allproduct = {}
+
+    def add_product(self, prod: Product):  # instance
+        try:
+            if type(prod) == Product:  # prod --> XXX : str --> validation --fail
+                if prod.prodId > 0:
+                    if prod.prodPrice >= 100.0:
+                        result = self.search_by_id(prod.prodId)  # Product or None
+                        if result:
+                            print('Product id already available')
+                            sys.exit()
+                        else:
+                            ProductServiceImpl.productList.append(prod)  # product will be added inside list-->
+                            print('Product Added Successfully...')
+                    else:
+                        print('Invalid Product ProductPrice')
+                else:
+                    print('Invalid Product Id')
+            else:
+                print('Invalid Product Type....Product Cannot be added..')
+        except MyError as err:
+            print(err.msg)
+        except:
+            print("There was error product was not added.")
+
+    def avg_product_price(self):
+        if ProductServiceImpl.productList:
+            try:
+                totalProductPrice = self.total_product_price()
+                avgPrice = totalProductPrice / len(ProductServiceImpl.productList)
+            except:
+                print("There was an error while calculating average..")
+            else:
+                return avgPrice
+
+    def delete_product(self, pid: int):
+        try:
+            if ProductServiceImpl.productList:
+                deleteList = []
+                for prod in ProductServiceImpl.productList:
+                    if pid == prod.prodId:
+                        deleteList.append(prod)
+                        ProductServiceImpl.productList.remove(prod)
+                        print("Successfully Deleted below Id: ")
+                return deleteList
+            else:
+                print("List is Empty")
+        except:
+            print("There was an error while removing data.")
+
+    def get_all_product(self):
+        try:
+            if ProductServiceImpl.productList:
+                return ProductServiceImpl.productList
+            else:
+                return ProductServiceImpl.productList
+        except:
+            print("There was an error while displaying all products")
+
+    def min_product_price(self):
+        try:
+            if ProductServiceImpl.productList:
+                priceList = []
+                for price in ProductServiceImpl.productList:
+                    priceList.append(price.prodPrice)
+                print('Minimum Product Price Value:', min(priceList))
+        except:
+            print("There was an error while calculating min product price")
+
+    def max_product_price(self):
+        try:
+            if ProductServiceImpl.productList:
+                priceList = []
+                for price in ProductServiceImpl.productList:
+                    priceList.append(price.prodPrice)
+                print('Maximum Product Price Value:', max(priceList))
+        except:
+            print("There was an error while calculating max price")
+
+    def search_by_category(self, cate: str):
+        try:
+            if ProductServiceImpl.productList:
+                prodcatlist = []
+                for cat in ProductServiceImpl.productList:
+                    if cat.prodCategory == cate:
+                        prodcatlist.append(cat)
+                return prodcatlist
+        except:
+            print("There was an error while searching category..")
+
+    def search_by_id(self, pid: int):  # If Product[Present] or  --> None[Absent]
+        try:
+            if ProductServiceImpl.productList:
+                for prod in ProductServiceImpl.productList:
+                    if prod.prodId == pid:
+                        return prod
+        except:
+            print("There was error..! While searching id in list.")
+
+    def search_by_vendor(self, ven: str):
+        try:
+            if ProductServiceImpl.productList:
+                vendorList = []
+                for prod in ProductServiceImpl.productList:
+                    if prod.prodVendor.vendorName == ven:
+                        vendorList.append(prod)
+                return vendorList
+        except:
+            print("There was an error while searching vendor..")
+
+    def total_product_price(self):
+        try:
+            if ProductServiceImpl.productList:
+                totalPrice = 0.0
+                for prod in ProductServiceImpl.productList:
+                    totalPrice = totalPrice + (prod.prodPrice * prod.prodQty)
+                return totalPrice
+        except:
+            print("There was an error while calculating total price..")
+
+    def update_product(self, pid: int):
+        try:
+            if ProductServiceImpl.productList:
+                prodToModify = pid
+                productList = ProductServiceImpl.productList
+                # search for the record to update
+                for prod in productList:
+
+                    if prodToModify == prod.prodId:
+                        break
+                    else:
+                        print("Record not found to modify.. please enter valid product id")
+                        # returning false if prod id is not available then control go to main menu.
+                        return False
+                try:
+                    option = int(input(
+                        '1. To modify name, 2. To modify price, '
+                        '3. To modify vendor, ''4. To modify category '
+                        '5. To modify vendor name ''6. To modify vendor address: '))
+                    if option == str:
+                        raise
+                except ValueError:
+                    print("Please Enter Integer Value only..")
+                except:
+                    print("Unaccepted response.. Please int value only")
+                else:
+                    if option == 1:
+                        try:
+                            prod.prodName = input("Enter to modify product name :")
+                            if prod.prodName == prod.prodName.isdigit():
+                                raise "Its required only string"
+                        except TypeError:
+                            print("Invalid product name")
+                        except:
+                            print("Its not valid product name..")
+                        else:
+                            productList.insert(prod.prodId, prod.prodName)
+                            print(prod)
+                            print("Updated Product Name Successfully.")
+                    elif option == 2:
+                        prod.prodPrice = float(input("Enter to modify Product price:"))
+                        productList.insert(prod.prodId, prod.prodPrice)
+                        print(prod)
+                        print("Updated Product Price Successfully")
+                    elif option == 3:
+                        prod.prodVendor = input("Enter to modify product vendor:")
+                        productList.insert(prod.prodId, prod.prodVendor)
+                        print(prod)
+                        print("Updated Vendor name successfully..")
+                    elif option == 4:
+                        prod.prodCategory = input("Enter to modify product category")
+                        productList.insert(prod.prodId, prod.prodCategory)
+                        print(prod)
+                        print("Updated prod category successfully.")
+                    elif option == 5:
+                        prod.prodVendor = input("Enter to modify product vendor:")
+                        productList.insert(prod.prodId, prod.prodVendor)
+                        print(prod)
+                        print("Updated Vendor Successfully")
+                    elif option == 6:
+                        prod.vendorAddr = input("Enter to modify vendor Address:")
+                        productList.insert(prod.prodId, prod.vendorAddr)
+                        print(prod)
+                        print("Updated vendor address successfully")
+                    else:
+                        print('Incorrect option selected.')
+        except:
+            print("There was an error while updating data..")
+
+    def product_names(self):
+        try:
+            productNames = []
+            if ProductServiceImpl.productList:
+                for prodnames in ProductServiceImpl.productList:
+                    productNames.append(prodnames.prodName)
+                return productNames
+        except:
+            print("There was an error while searching product names")
+
+    def product_quantity(self, pname):
+        try:
+            quantity = {}
+            totalQty = 0
+            if ProductServiceImpl.productList:
+                for qty in ProductServiceImpl.productList:
+
+                    if qty.prodName == pname:
+                        totalQty += qty.prodQty
+                        quantity[pname] = totalQty
+                    else:
+                        print("Product is not available..")
+            return quantity
+
+        except:
+            print("There was an error while displaying quantity..")
+
+    def display_all_product_quantity(self):
+        try:
+
+            if ProductServiceImpl.productList:
+                for product in ProductServiceImpl.productList:
+                    ProductServiceImpl.allproduct[product.prodName] = ProductServiceImpl.allproduct.get(
+                        product.prodName, 0) + product.prodQty
+                for product, prodval in ProductServiceImpl.allproduct.items():
+                    print(str(product).title(), "Available Quantity is:", prodval)
+                return ProductServiceImpl.allproduct
+
+        except:
+            print("There was an error while displaying all products..")
+
+    def withdraw_product_quantity(self, pname):
+        try:
+
+            if ProductServiceImpl.productList:
+                withdrawproduct = int(input("Please Enter How many products you want to buy? "))
+                totalproduct = self.product_quantity(pname)
+                print(str(pname).title(), "Available Quantity: ", totalproduct)
+                try:
+                    if len(totalproduct.values()) < 0:
+                        raise MyError("Product is not available for selling..")
+                except MyError as err:
+                    print(err.msg)
+                else:
+                    for key, value in totalproduct.items():
+                        if key == pname:
+                            ProductServiceImpl.allproduct[key] = ProductServiceImpl.allproduct.get(key,
+                                                                                                   0) - withdrawproduct
+                    print(str(pname).title(), "Available quantity After Selling: ", ProductServiceImpl.allproduct)
+        except:
+            print("There was an error while withdrawing product..")
---
+++ prodinventory/main.py
@@ -0,0 +1,152 @@
+import sys
+from myerror import MyError
+from ecominventory.impl import ProductServiceImpl, Product, ValueDivision
+from ecominventory.vendorinfo import Vendor
+
+
+def take_product_input():
+    def take_vendor_input():
+        """Take input from User and create Vendor Object"""
+        try:
+            vid = int(input('Enter Vendor Id : '))
+            vname = str(input('Enter Vendor Name : '))
+            if vname.isdigit() or vname.isspace():
+                raise MyError("unaccepted response.. please enter string only")
+            vadr = str(input('Enter Vendor Address : '))
+            if vadr.isdigit() or len(vadr) < 4:
+                raise MyError("unaccepted response.. please enter string only")
+        except ValueError:
+            print("Please enter integer value.")
+        except TypeError as msg:
+            print("Letters only please.", msg)
+        except MyError as err:
+            print(err.msg)
+        return Vendor(vid=vid, vnm=vname, vadr=vadr)
+
+    '''Take input from user --> and create product object...'''
+    try:
+        pid = int(input('Enter Product Id : '))
+        pnam = str(input('Enter Product Name : '))
+        if pnam.isdigit() or pnam.isspace():
+            raise MyError("Unaccepted response. Please enter valid product name")
+        pqty = int(input('Enter Product Qty : '))
+        prc = float(input('Enter Product Price : '))
+        ven = take_vendor_input()
+        cat = str(input('Enter Product Category : '))
+        if cat.isdigit() or cat.isspace():
+            raise MyError("unaccepted response.. please enter string only")
+        return Product(pid=pid, pnm=pnam, pric=prc, pqty=pqty, pven=ven, pcat=cat)
+    except ValueError:
+        print("Value should be number")
+    except TypeError as msg:
+        print("Letters only please ", msg)
+    except MyError as err:
+        print(err.msg)
+    except:
+        print("Please provide valid input")
+
+
+if __name__ == '__main__':
+    prodService = ProductServiceImpl(ValueDivision)
+    while True:
+        print('''
+                1 Add_product
+                2 Avg_product_price
+                3 Delete_product
+                4 Get_all_product
+                5 Min_product_price
+                6 Max_product_price
+                7 Search_by_category
+                8 Search_by_id
+                9 Search_by_vendor
+                10 Total_product_price
+                11 Update_product
+                12 Product Names
+                13 Product Quantity
+                14 Display All Product Quantity
+                15 Withdraw Product Quantity
+                16 Exit
+       
+        ''')
+        try:
+            choice = int(input('Enter Your Choice : '))
+        except ValueError:
+            print("Enter Valid Choice. Please Try again..")
+            continue
+        if choice == 1:
+            product = take_product_input()
+            prodService.add_product(product)
+        elif choice == 2:
+            avg = prodService.avg_product_price()
+            print('Total Product Price : ', avg)
+        elif choice == 3:
+            try:
+                pid = int(input("Enter product id to delete? "))
+                print(prodService.delete_product(pid))
+            except ValueError as v:
+                print("Please enter valid product id")
+
+        elif choice == 4:
+            prodList = prodService.get_all_product()
+            print('Products', prodList)
+        elif choice == 5:
+            prodService.min_product_price()
+        elif choice == 6:
+            prodService.max_product_price()
+        elif choice == 7:
+            try:
+                cat = input("Enter category which one you want to search?")
+                if cat.isdigit():
+                    raise MyError("Unaccepted response enter valid input")
+                print(prodService.search_by_category(cat))
+            except MyError as err:
+                print(err.msg)
+        elif choice == 8:
+            try:
+                pid = int(input("Enter Product Id which one you want to search:"))
+                print(prodService.search_by_id(pid))
+            except ValueError as v:
+                print("Please int value")
+        elif choice == 9:
+            try:
+                ven = input("Enter Vendor Name which vendor you want to search: ")
+                if ven.isdigit():
+                    raise MyError("Unaccepted response. Please enter valid input")
+                print(prodService.search_by_vendor(ven))
+            except MyError as err:
+                print(err.msg)
+        elif choice == 10:
+            total = prodService.total_product_price()
+            print("Total Price of Product:", total)
+        elif choice == 11:
+            try:
+                pid = int(input("Enter the product id:"))
+                prodService.update_product(pid)
+            except ValueError as v:
+                print("Please enter int value only", v)
+        elif choice==12:
+
+            print(prodService.product_names())
+        elif choice==13:
+            pname=input("Enter product name: ")
+            print(prodService.product_quantity(pname))
+        elif choice==14:
+            print(prodService.display_all_product_quantity())
+        elif choice==15:
+            pname=input("Please enter which product you want to buy..")
+            prodService.withdraw_product_quantity(pname)
+        elif choice == 16:
+            print("Thanks for using Inventory Application..")
+            sys.exit(0)
+        else:
+            print('Invalid Choice..')
+
+        ch = str(input('Do you want to continue : Y/N'))
+        try:
+            if ch.lower() in ['n', 'no']:
+                if ch.isdigit() or ch.isspace():
+                    raise MyError("Unaccepted response please try again")
+                print("Thanks for visit..")
+                break
+        except MyError as err:
+            print(err.msg)
