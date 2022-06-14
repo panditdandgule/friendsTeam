4dac6c8a0f37022f9591d8ab310cfb6016156ee1
pandit dandgule <panditdandgule777@gmail.com>
2022-06-14 21:03:14 +0530

    added test cases.

--- friendsTeam/pandit/myproject/boards/urls.py
+++ friendsTeam/pandit/myproject/boards/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path,re_path
 from boards import views
 
 
 urlpatterns = [
 
-    path('',views.home,name='home'),
+    re_path(r'^$',views.home,name='home'),
+    re_path(r'^boards/(?P<pk>\d+)/$',views.board_topics,name='board_topics'),
+
 ]
+
--- friendsTeam/pandit/myproject/boards/views.py
+++ friendsTeam/pandit/myproject/boards/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse, Http404
 from boards.models import Board
 
 # Create your views here.
@@ -9,3 +9,13 @@ def home(request):
        'boards':boards
    }
    return render(request,'home.html',context)
+
+def board_topics(request,pk):
+    # try:
+    #     board=Board.objects.get(pk=pk)
+    # except Board.DoesNotExist:
+    #     raise Http404
+
+    board=get_object_or_404(Board,pk=pk)
+    return render(request,'topics.html',{'board':board})
+
