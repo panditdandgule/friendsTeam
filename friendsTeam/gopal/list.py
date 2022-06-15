129f561bf86465e84a04f7c2afc773bf3fd5240d
Gopal <mitkarigopal@gmail.com>
2022-06-15 19:43:28 +0530

    added file

---
+++ friendsTeam/gopal/list.py
@@ -0,0 +1,30 @@
+"""def main():
+    name_list = []
+    again = 'y'
+    while again == 'y':
+        name = input('Enter a name: ')
+        name_list.append(name)
+        print('Do you want to add another name?')
+        again = input('y=yes,anything else = no:')
+        print()
+    print('here are the names you entered.')
+    for name in name_list:
+        print(name)
+if __name__ =='__main__':
+    main()"""
+
+"""def main():
+    numbers = [2,4,6,8,10]
+    total = 0
+    for value in numbers:
+        total += value
+    print(f'The total of the element is {total}.')
+if __name__ == '__main__':
+    main()
+"""
+
+
+
+
+
+
